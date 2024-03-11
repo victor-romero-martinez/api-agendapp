e0c909844a34a54caf9e3ff7edcd75d43429baa2
victor-romero-martinez <victordav147@gmail.com>
2024-03-11 12:38:55 -0300

    [add] task model

--- server-with-sqlite3.mjs
+++ server-with-sqlite3.mjs
@@ -1,6 +1,8 @@
 import { createApp } from "./index.mjs";
+import { Task } from "./models/sqlite/tasks.model.mjs";
 import { User } from "./models/sqlite/user.model.mjs";
 
 const userModel = new User();
+const taskModel = new Task();
 
-createApp({ userModel });
+createApp({ userModel, taskModel });
