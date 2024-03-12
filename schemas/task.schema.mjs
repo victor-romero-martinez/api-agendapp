26a4372626d9280fd716fa9fbabfa2c337fb24eb
victor-romero-martinez <victordav147@gmail.com>
2024-03-12 10:23:35 -0300

    [fix] priority

--- schemas/task.schema.mjs
+++ schemas/task.schema.mjs
@@ -1,13 +1,12 @@
 import z from "zod";
 
 const status = ["pending", "inprogress", "completed"];
-const priority = [1, 2, 3, 4, 5];
 
 export const taskSchema = z.object({
   title: z.string().min(4).max(60),
   description: z.string().min(4).max(250).optional(),
   status: z.enum(status).optional(),
-  priority: z.enum().optional(),
+  priority: z.number().min(1).max(5).optional(),
   updated_at: z.string().optional(),
   due_date: z.string().optional(),
 });
