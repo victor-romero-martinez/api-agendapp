2730ec76fec4dd17de8fd32e5a84cdefd5463fb3
victor-romero-martinez <victordav147@gmail.com>
2024-03-12 17:56:57 -0300

    [yp] add editable schema

--- schemas/task.schema.mjs
+++ schemas/task.schema.mjs
@@ -5,8 +5,11 @@ const status = ["pending", "inprogress", "completed"];
 export const taskSchema = z.object({
   title: z.string().min(4).max(60),
   description: z.string().min(4).max(250).optional(),
-  status: z.enum(status).optional(),
   priority: z.number().min(1).max(5).optional(),
-  updated_at: z.string().optional(),
   due_date: z.string().optional(),
+});
+
+export const taskEditable = taskSchema.extend({
+  id: z.number(),
+  status: z.enum(status).optional(),
 });
