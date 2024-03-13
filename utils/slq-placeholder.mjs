d6e43e407dcfdcb2f04d76df536f7d1ee9465990
victor-romero-martinez <victordav147@gmail.com>
2024-03-13 11:30:49 -0300

    [up] add variant to make placeholders

--- utils/slq-placeholder.mjs
+++ utils/slq-placeholder.mjs
@@ -1,14 +1,19 @@
 //@ts-check
 /** SQL placeholder fn
  * @param {Record<string, string>} args - object
+ * @param {'INSERT'|'UPDATE'} variant - variant
  * @returns {[string, string[]]}
  */
-export function placeholderQuery(args) {
+export function placeholderQuery(args, variant = "INSERT") {
   let placeholder = [];
   let params = [];
 
   for (const [key, value] of Object.entries(args)) {
-    placeholder.push(key);
+    if (variant == "UPDATE") {
+      placeholder.push(`${key} = ?`);
+    } else {
+      placeholder.push(key);
+    }
     params.push(value);
   }
   return [placeholder.toString(), params];
