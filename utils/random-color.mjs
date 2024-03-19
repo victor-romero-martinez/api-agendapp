12d289f72aadb30d646eab58f5b1e794d2216dce
victor-romero-martinez <victordav147@gmail.com>
2024-03-19 19:30:34 -0300

    refactor

--- utils/random-color.mjs
+++ utils/random-color.mjs
@@ -3,8 +3,14 @@ export function randomColor() {
   let random = [];
 
   while (random.length < 3) {
-    random.push(Math.floor(Math.random() * 100) + 1);
+    // Rango de valores más acotado
+    random.push(Math.floor(Math.random() * 60) + 180); // Tono entre 180° y 240°
+    random.push(Math.floor(Math.random() * 40) + 30); // Blancura entre 30% y 70%
+    random.push(Math.floor(Math.random() * 40) + 30); // Negro entre 30% y 70%
   }
+
+  // Distribución normal para la saturación
+  random[1] = Math.floor(random[1] + (Math.random() - 0.5) * 20);
 
   return `hwb(${random[0]}deg ${random[1]}% ${random[2]}%)`;
 }
